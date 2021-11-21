d0fc14c9c1dcd3734c9ad7edf32e88820e8d9ce8
陈辉 <unmerged@163.com>
2021-11-21 16:55:57 +0800

    添加 README

--- cmd/examples/simplest_example.go
+++ cmd/examples/simplest_example.go
@@ -1,26 +1,7 @@
-昆仑代码搜索引擎
-======
-
-* 提供了 [KWS](/cmd/kws) 网页搜索界面
-* 提供了 [KLS](/cmd/kls)，一个命令行界面的代码搜索瑞士军刀程序
-* 采用对商业应用友好的[Apache License v2](/LICENSE)发布
-
-# 安装/更新
-
-```
-go get -u -v github.com/huichen/kunlun
-```
-
-# 使用
-
-先看一个例子（来自[cmd/examples/simplest_example.go](/cmd/examples/simplest_example.go)）
-
-```go
 package main
 
 import (
 	"flag"
-	"log"
 
 	"kunlun/pkg/engine"
 	"kunlun/pkg/types"
@@ -51,11 +32,3 @@ func main() {
 	// 打印输出
 	kgn.PrettyPrintSearchResponse(resp, true, true)
 }
-```
-
-是不是很简单！
-
-
-# 其它
-
-* [联系方式](/doc/feedback.md)
